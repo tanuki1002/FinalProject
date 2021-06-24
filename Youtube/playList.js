0a433def4d19b4937910a14854aecb3f174c807b
Tannuki Lee <tanukilee102@gmail.com>
2021-06-24 16:04:26 +0800

    first commit

---
+++ Youtube/playList.js
@@ -0,0 +1,14 @@
+//YouTube Video ID
+let playList = [
+    "KdxSAaEEhM4", //夜に駆ける
+    "G-KSTWO5ir0", //Danse Macabre
+    "TBqujyl4ja0" //戀音と雨空
+];
+
+//播放起訖秒數
+let playTime = [
+    [101, 109],
+    [144, 150],
+    [146, 152]
+
+];
